22ea1e61ab27292a7b3b21de6a1e4ef1228612c7
Dr1lla <151372221+Dr1lla@users.noreply.github.com>
2024-05-25 18:06:14 +0300

    TT2

--- src/components/user/UserComponent.tsx
+++ src/components/user/UserComponent.tsx
@@ -1,12 +1,14 @@
 import React, {ReactNode, FC} from 'react';
 import {IPropsUsers} from "../interfaces/User.interface";
 
-type UserComponentWithChildren<T> = T & { children?: ReactNode };
-const UserComponent: FC<UserComponentWithChildren<IPropsUsers>> = ({ id, name, email, userName, children }) => {
+type UserComponentWithChildren<T> = T & { children?: ReactNode } & { clickHandler: (id:number) => void};
+const UserComponent: FC<UserComponentWithChildren<IPropsUsers>> = ({ id, name, email, userName, clickHandler}) => {
     return (
         <div>
             <div>{id} - {name} Username - {userName} Email - {email}</div>
-            {children}
+            <button onClick={() =>{
+                clickHandler(id)
+            }}>Chose</button>
         </div>
     );
 };
--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -18,6 +18,13 @@ const Users: FC = () => {
         }
     }, []);
 
+    const [userId, setUserId] = useState<number>(0)
+
+    const clickHandler = (id:number) => {
+        setUserId(id);
+        console.log(id);
+    }
+
     return (
         <>
             {
@@ -27,9 +34,11 @@ const Users: FC = () => {
                         name={name}
                         userName={userName}
                         email={email}
+                        clickHandler={clickHandler}
                     />
                 ))
             }
+            <p>{userId}</p>
         </>
     );
 };
