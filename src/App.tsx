4611aefd36af115e47a5537b9468cc1a6cbb9e7e
Dr1lla <151372221+Dr1lla@users.noreply.github.com>
2024-06-05 16:05:27 +0300

    TT5: Done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    let [number, setNumber] = useState<number>(0)
+
+    const increment = () => {
+        setNumber(prevState => prevState + 1);
+        console.log(number)
+    }
   return (
       <div>
-
+          <h2>{number}</h2>
+          <button onClick={increment}> Increment</button>
       </div>
   );
 }
